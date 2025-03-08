6274521e0734cbe47e7c9d8968aba230388bd400
Ash Monsh <wh7r.com@gmail.com>
2025-03-09 02:59:15 +0300

    Update BoltPlugin.php

--- src/BoltPlugin.php
+++ src/BoltPlugin.php
@@ -29,6 +29,8 @@ final class BoltPlugin implements Plugin
 
     protected Closure | string $navigationGroupLabel = 'Bolt';
 
+    // test
+    
     public array $defaultGloballySearchableAttributes = [
         CategoryResource::class => ['name', 'slug'],
         CollectionResource::class => ['name', 'values'],
